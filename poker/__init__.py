226f71b6910e0a7024b27291845acea54c247dcf
oangia <hqnhatdn@gmail.com>
2025-03-29 13:00:41 +0700

    Update __init__.py

--- poker/__init__.py
+++ poker/__init__.py
@@ -1,15 +1,5 @@
 import random
 
-class Card:
-    def __init__(self, card):
-        self.rank = int(card[:-1])
-        self.suit = card[-1]
-        self.point = self.rank - 2 if self.rank != 1 else 12
-        self.power = 2 ** self.point
-
-    def __repr__(self):
-        return f"'{self.rank}{self.suit}'"
-
 class Hand:
     ZITCH = 0
     ONEPAIR = 1
@@ -106,62 +96,6 @@ class Hand:
     def __repr__(self):
         #print({"Hand": ",".join([f"{card.rank}{card.suit}" for card in self.cards]), "type": self.getHandType(), "point": self.point, "zitch point": self.zitch_point})
         return ",".join([f"{card.rank}{card.suit}" for card in self.cards])
-
-class Deck:
-    def __init__(self):
-        suits = ['s', 'c', 'd', 'h']
-        ranks = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
-        self.cards = [f"{rank}{suit}" for rank in ranks for suit in suits]
-
-    def dealRemain(self, players):
-        cards = []
-        for card in self.cards:
-            if card in players:
-                continue
-            cards.append(card)
-        return cards
-
-    def getCards(self, cards):
-        return [Card(card) for card in cards.split(",")]
-
-    def get_random_cards(self, count = 13):
-        return sorted(random.sample(self.cards, count),
-                      key=lambda card: card.rank)
-
-    def get_hand(self, type, loop = 1):
-        for i in range(loop):
-            count = 0
-            while True:
-                count += 1
-                cards = self.get_random_cards(5)
-                hand = Hand(cards)
-                if hand.getHandType() == type:
-                    print(",".join([f"{card.rank}{card.suit}" for card in cards]), {"type": hand.getHandType(), "point": hand.point, "zitch point": hand.zitch_point}, count)
-                    break
-
-    def testHands(self):
-        self.get_hand("ZITCH")
-        self.get_hand("ONEPAIR")
-        self.get_hand("TWOPAIR")
-        self.get_hand("THREEKIND")
-        self.get_hand('STRAIGHT')
-        self.get_hand('FLUSH')
-        self.get_hand("FULLHOUSE")
-        self.get_hand("FOURKIND")
-        self.get_hand("STRAIGHT_FLUSH")
-
-    def shuffle(self):
-        random.shuffle(self.cards)
-
-    def draw(self):
-        """Draws a card from the deck. Returns None if the deck is empty."""
-        return self.cards.pop() if self.cards else None
-
-    def __len__(self):
-        return len(self.cards)
-
-    def __repr__(self):
-        return f"Deck({self.cards})"
 
 class Game:
     def __init__(self):
