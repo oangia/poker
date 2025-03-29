2fa3aaab606762b6a63d49ca95ab1278db23b219
oangia <hqnhatdn@gmail.com>
2025-03-29 13:08:33 +0700

    Create Setting.py

---
+++ poker/Setting.py
@@ -0,0 +1,8 @@
+class Setting:
+    def __init__(self, back, middle, front):
+        self.back = back
+        self.middle = middle
+        self.front = front
+
+    def compare(opponent):
+        return 1
