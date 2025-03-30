3583f5f160b167580a7f0414bcf1ee36e9861088
oangia <hqnhatdn@gmail.com>
2025-03-30 15:21:25 +0700

    Update Setting.py

--- poker/Setting.py
+++ poker/Setting.py
@@ -9,3 +9,8 @@ class Setting:
             return [self.back.compare(opponent.back), self.middle.compare(opponent.middle), self.front.compare(opponent.front)]
         elif resType == "sum":
             return sum([self.back.compare(opponent.back), self.middle.compare(opponent.middle), self.front.compare(opponent.front)])
+
+    def checkValid():
+        if self.back.point >= self.middle.point and self.middle.point > self.front.point:
+            return True
+        return False
