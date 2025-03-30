654117754c5564e18d9473db4f1353920736d463
oangia <hqnhatdn@gmail.com>
2025-03-30 15:56:45 +0700

    Update Game.py

--- poker/Game.py
+++ poker/Game.py
@@ -1,11 +1,10 @@
 from poker.Deck import Deck
+from poker.Player import Player
 
 class Game:
     def __init__(self):
         self.deck = Deck()
-
-    def setPlayerClass(self, className):
-        self.playerClass = className
+        
     def helper(self):
         print("Generate cards: poker.generateFullHand()")
         print("Generate hand of five: poker.generateHand(5)")
@@ -13,11 +12,11 @@ class Game:
 
     def encirclement(self, cards):
         players = cards.split("|")
-        player1 = self.playerClass(players[0])
-        player2 = self.playerClass(players[1])
-        player3 = self.playerClass(players[2])
+        player1 = Player(players[0])
+        player2 = Player(players[1])
+        player3 = Player(players[2])
         opponentCards = self.deck.dealRemain(players[0].split(",") + players[1].split(",") + players[2].split(","))
-        opponent = self.playerClass(opponentCards)
+        opponent = Player(opponentCards)
         opponent.getStrongestSetting()
         p1Setting = player1.getBestFitSetting(opponent)
         p2Setting = player2.getBestFitSetting(opponent)
@@ -25,7 +24,7 @@ class Game:
         return p1Setting + "|" + p2Setting + "|" + p3Setting
     
     def single(self, cards):
-        player1 = self.playerClass(cards)
+        player1 = Player(cards)
         return player1.getStrongestSetting()
 
     def generateFullHand(self):
