fad227d7fc07b095f971b6d29563104d1d3e5004
oangia <hqnhatdn@gmail.com>
2025-03-30 23:06:49 +0700

    Delete poker/mypoker.py

--- poker/mypoker.py
+++
@@ -1,9 +0,0 @@
-class Card:
-    def __init__(self, card):
-        self.rank = int(card[:-1])
-        self.suit = card[-1]
-        self.point = self.rank - 2 if self.rank != 1 else 12
-        self.power = 2 ** self.point
-
-    def __repr__(self):
-        return f"'{self.rank}{self.suit}'"
