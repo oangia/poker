c0c2d6489ee2b9f01e541590be655682422d18af
oangia <hqnhatdn@gmail.com>
2025-03-31 12:31:53 +0700

    Update Poker.py

--- poker/Poker.py
+++ poker/Poker.py
@@ -1,7 +1,6 @@
 from itertools import combinations
 import random
-from poker import Card, Setting
-from poker.Hand import Hand
+from poker.base import Card, Hand, Setting
 class Poker:
     CARDS = ['1s', '1c', '1d', '1h', '2s', '2c', '2d', '2h', '3s', '3c', '3d', '3h', '4s', '4c', '4d', '4h', '5s', '5c', '5d', '5h', '6s', '6c', '6d', '6h', '7s', '7c', '7d', '7h', '8s', '8c',
          '8d', '8h', '9s', '9c', '9d', '9h', '10s', '10c', '10d', '10h', '11s', '11c', '11d', '11h', '12s', '12c', '12d', '12h', '13s', '13c', '13d', '13h']
