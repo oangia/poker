e5d336b079ec2884635df9a4afedefbfda810160
oangia <hqnhatdn@gmail.com>
2025-04-01 15:52:32 +0700

    Update and rename poker/base/Hand/Hand.py to poker/base/Hand.py

--- poker/base/Hand.py
+++ poker/base/Hand.py
@@ -1,6 +1,14 @@
-from poker.base.Hand.HandType import HandType
-
 class Hand:
+    ZITCH = 0
+    ONEPAIR = 1
+    TWOPAIR = 2
+    THREEKIND = 3
+    STRAIGHT = 4
+    FLUSH = 5
+    FULLHOUSE = 6
+    FOURKIND = 7
+    STRAIGHT_FLUSH = 8
+    
     def __init__(self, cards):
         self.cards = cards
         self.point = self.detect()
@@ -19,18 +27,18 @@ class Hand:
         self.power = sum([card.power for card in self.cards])
         self.handType = 0
         self.zitch_point = 0
-        self.handType = TypeDetect.detect(self.cards)
+        self.handType = self.typeDetect()
         self.point = self.calculatePoint()
         return self.handType * 10 + self.point
         
     def calculatePoint(self):
-        if self.handType in [HandType.ZITCH, HandType.FLUSH, HandType.STRAIGHT, HandType.STRAIGHT_FLUSH]:
+        if self.handType in [Hand.ZITCH, Hand.FLUSH, Hand.STRAIGHT, Hand.STRAIGHT_FLUSH]:
             return self.power/self.MAX_ZITCH_POWER
 
-        if self.handType in [HandType.THREEKIND, HandType.FULLHOUSE, HandType.FOURKIND]:
+        if self.handType in [Hand.THREEKIND, Hand.FULLHOUSE, Hand.FOURKIND]:
             return self.cards[2].point
 
-        if self.handType == HandType.ONEPAIR:
+        if self.handType == Hand.ONEPAIR:
             if self.cards[0].rank == self.cards[1].rank:
                 self.zitch_point = (self.cards[2].power + self.cards[3].power + self.cards[4].power)/self.MAX_ZITCH_POWER
                 return self.cards[0].point
@@ -44,7 +52,7 @@ class Hand:
                 self.zitch_point = (self.cards[0].power + self.cards[1].power + self.cards[2].power)/self.MAX_ZITCH_POWER
                 return self.cards[4].point
 
-        if self.handType == HandType.TWOPAIR:
+        if self.handType == Hand.TWOPAIR:
             if self.cards[0].rank != self.cards[1].rank:
                 self.zitch_point = self.cards[0].power/self.MAX_POWER
             elif self.cards[3].rank != self.cards[4].rank:
@@ -54,24 +62,24 @@ class Hand:
 
             return (self.cards[1].power + self.cards[3].power)/self.MAX_ZITCH_POWER
 
-    def detect():
+    def typeDetect(self):
         ranks = [card.rank for card in self.cards]
         count = len(set(ranks))
 
         if count < 5:
             if count == 4:
-                return HandType.ONEPAIR
+                return Hand.ONEPAIR
 
             if count == 3:
                 if ranks[0] == ranks[2] or ranks[1] == ranks[3] or ranks[2] == ranks[4]:
-                    return HandType.THREEKIND
-                return HandType.TWOPAIR
+                    return Hand.THREEKIND
+                return Hand.TWOPAIR
 
             if count == 2:
                 if ranks[1] == ranks[3]:
-                    return HandType.FOURKIND
-                return HandType.FULLHOUSE
+                    return Hand.FOURKIND
+                return Hand.FULLHOUSE
 
         flush = int(len(set([card.suit for card in self.cards])) == 1)
         straight = int(ranks[4] - ranks[0] == 4 or (ranks[0] == 1 and ranks[1] == 10))
-        return straight * HandType.STRAIGHT + flush * HandType.FLUSH - flush * straight
+        return straight * Hand.STRAIGHT + flush * Hand.FLUSH - flush * straight
