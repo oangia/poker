b52592438fda494e8183c44a355924c855acc819
oangia <hqnhatdn@gmail.com>
2025-04-01 14:02:09 +0700

    Update __init__.py

--- poker/HandDetect/__init__.py
+++ poker/HandDetect/__init__.py
@@ -1,15 +1,6 @@
 from poker.base import HandType
 
 class HandDetect:
-    ZITCH = 0
-    ONEPAIR = 1
-    TWOPAIR = 2
-    THREEKIND = 3
-    STRAIGHT = 4
-    FLUSH = 5
-    FULLHOUSE = 6
-    FOURKIND = 7
-    STRAIGHT_FLUSH = 8
     MAX_POWER = 4097
     MAX_ZITCH_POWER = 7937
 
@@ -20,45 +11,10 @@ class HandDetect:
         self.cards = cards
         self.handType = 0
         self.zitch_point = 0
-        self.handType = self.detectHand()
+        self.handType = TypeDetect.detect(self.cards)
         self.point = self.calculatePoint()
         return self.handType * 10 + self.point
-
-    def detectHand(self):
-        ranks = [card.rank for card in self.cards]
-        count = len(set(ranks))
-
-        if count < 5:
-            if count == 4:
-                return self.ONEPAIR
-
-            if count == 3:
-                if ranks[0] == ranks[2] or ranks[1] == ranks[3] or ranks[2] == ranks[4]:
-                    return self.THREEKIND
-                return self.TWOPAIR
-
-            if count == 2:
-                if ranks[1] == ranks[3]:
-                    return self.FOURKIND
-                return self.FULLHOUSE
-
-        suits = [card.suit for card in self.cards]
-        self.power = sum([card.power for card in self.cards])
-        self.flush = len(set(suits)) == 1
-        self.straight = self.power in [4111, 31, 62, 124, 248, 496, 992, 1984, 3968, 7936]
-        self.straightFlush = False
-
-        if self.straight and self.flush:
-            return self.STRAIGHT_FLUSH
-
-        if self.straight:
-            return self.STRAIGHT
-
-        if self.flush:
-            return self.FLUSH
-
-        return self.ZITCH
-
+        
     def calculatePoint(self):
         if self.handType in [self.ZITCH, self.FLUSH, self.STRAIGHT, self.STRAIGHT_FLUSH]:
             return self.power/self.MAX_ZITCH_POWER
