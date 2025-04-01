d5b1273a2707105147d1d684882397f39c2ac845
oangia <hqnhatdn@gmail.com>
2025-04-01 14:03:48 +0700

    Update __init__.py

--- poker/HandDetect/__init__.py
+++ poker/HandDetect/__init__.py
@@ -16,13 +16,13 @@ class HandDetect:
         return self.handType * 10 + self.point
         
     def calculatePoint(self):
-        if self.handType in [self.ZITCH, self.FLUSH, self.STRAIGHT, self.STRAIGHT_FLUSH]:
+        if self.handType in [HandType.ZITCH, HandType.FLUSH, HandType.STRAIGHT, HandType.STRAIGHT_FLUSH]:
             return self.power/self.MAX_ZITCH_POWER
 
-        if self.handType in [self.THREEKIND, self.FULLHOUSE, self.FOURKIND]:
+        if self.handType in [HandType.THREEKIND, HandType.FULLHOUSE, HandType.FOURKIND]:
             return self.cards[2].point
 
-        if self.handType == self.ONEPAIR:
+        if self.handType == HandType.ONEPAIR:
             if self.cards[0].rank == self.cards[1].rank:
                 self.zitch_point = (self.cards[2].power + self.cards[3].power + self.cards[4].power)/self.MAX_ZITCH_POWER
                 return self.cards[0].point
@@ -36,7 +36,7 @@ class HandDetect:
                 self.zitch_point = (self.cards[0].power + self.cards[1].power + self.cards[2].power)/self.MAX_ZITCH_POWER
                 return self.cards[4].point
 
-        if self.handType == self.TWOPAIR:
+        if self.handType == HandType.TWOPAIR:
             if self.cards[0].rank != self.cards[1].rank:
                 self.zitch_point = self.cards[0].power/self.MAX_POWER
             elif self.cards[3].rank != self.cards[4].rank:
