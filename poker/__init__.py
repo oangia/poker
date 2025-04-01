11ba1483fdc6b788643f3465bfb22b8c7091b87c
oangia <hqnhatdn@gmail.com>
2025-04-01 13:24:11 +0700

    Update __init__.py

--- poker/__init__.py
+++ poker/__init__.py
@@ -1,2 +1,10 @@
 from poker.base import Card, Hand, Setting, Player
 from poker.helper import Deck, Poker
+import time
+def testFunc(func, loop = 10):
+    results = []
+    for i in range(loop):
+        start = time.time()
+        testFunc()
+        results.append(time.time()-start)
+    print(results, sum(results)/len(results))
