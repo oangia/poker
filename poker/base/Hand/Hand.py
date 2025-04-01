f3f9714007cce35d165e7dca064b1f663968cd43
oangia <hqnhatdn@gmail.com>
2025-04-01 15:41:45 +0700

    Create Hand.py

---
+++ poker/base/Hand/Hand.py
@@ -0,0 +1,14 @@
+class Hand:
+    def __init__(self, cards, handDetect):
+        self.cards = cards
+        self.point = handDetect.detect(self.cards)
+        
+    def compare(self, opponent):
+        if self.point > opponent.point:
+            return 1
+        if self.point < opponent.point:
+            return -1
+        return 0
+
+    def toStr(self):
+        return ",".join(card.toStr() for card in self.cards)
