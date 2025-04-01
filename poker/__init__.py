1d46685eafe882ff2960b51816480e2d71cf5a0e
oangia <hqnhatdn@gmail.com>
2025-04-01 16:06:11 +0700

    Update __init__.py

--- poker/__init__.py
+++ poker/__init__.py
@@ -3,45 +3,13 @@ from poker.Hand import Hand
 from poker.Setting import Setting
 from poker.Player import Player
 from poker.Deck import Deck
-
-import time as timer
-class Poker:
-    timeStart = 0
-            
-    @staticmethod
-    def testFunc(func, loop = 10):
-        results = []
-        for i in range(loop):
-            start = time.time()
-            func()
-            results.append(time.time()-start)
-        print(results, sum(results)/len(results))
-    @staticmethod
-    def testHand(pokerClass, handType, loop = 5):
-        poker = pokerClass()
-        count = 1
-        for i in range(loop):
-            cards = Poker.randomCards()
-            point = poker.detect(cards)
-            while handType != poker.getHandType(point):
-                count += 1
-                cards = Poker.randomCards()
-                point = poker.detect(cards)
-            print(poker.getHandType(point), cards, count, point)
-
-    @staticmethod
-    def testHands(pokerClass):
-        for handType in Poker.HANDS:
-            Poker.testHand(pokerClass, handType, 1)
+from poker.Poker import Poker
 
 def time():
     Poker.timeStart = timer.time()
 
 def track(msg=""):
-    if msg=="":
-        print(timer.time()-Poker.timeStart)
-    else:
-        print(msg + ": " + str(timer.time()-Poker.timeStart))
+    Poker.track(msg)
 
 def getStrongestSetting(cards):
     player = Player(cards)
