5a5797eb17dbde69543dabf20ba9a74c1672a56c
oangia <hqnhatdn@gmail.com>
2025-04-01 15:41:35 +0700

    Update __init__.py

--- poker/base/Hand/__init__.py
+++ poker/base/Hand/__init__.py
@@ -1,17 +1,3 @@
 from poker.base.Hand.HandType import HandType
 from poker.base.Hand.TypeDetect import TypeDetect
-
-class Hand:
-    def __init__(self, cards, handDetect):
-        self.cards = cards
-        self.point = handDetect.detect(self.cards)
-        
-    def compare(self, opponent):
-        if self.point > opponent.point:
-            return 1
-        if self.point < opponent.point:
-            return -1
-        return 0
-
-    def toStr(self):
-        return ",".join(card.toStr() for card in self.cards)
+from poker.base.Hand.Hand import Hand
