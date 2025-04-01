3faa75c5b9f9b9e9aa6594de0f24c27dda9f6378
oangia <hqnhatdn@gmail.com>
2025-04-01 14:08:16 +0700

    Create Card.py

---
+++ poker/base/Card.py
@@ -0,0 +1,11 @@
+class Card:
+    def __init__(self, card):
+        self.rank = int(card[:-1])
+        self.suit = card[-1]
+        self.point = self.rank - 2 if self.rank != 1 else 12
+        self.power = 2 ** self.point
+
+    def toStr(self):
+        return f"{self.rank}{self.suit}"
+    def __repr__(self):
+        return f"{self.rank}{self.suit}"
