3c712cde2b6b0c3d2991aec02add18b3b1297aa4
oangia <hqnhatdn@gmail.com>
2025-04-01 14:19:07 +0700

    Update Player.py

--- poker/base/Player.py
+++ poker/base/Player.py
@@ -4,15 +4,14 @@ from poker.base.BruteForce import BruteForce
 class Player:
     def __init__(self, cards):
         self.cards = [Card(card) for card in cards]
-        self.algo = BruteForce(self.cards)
-
-    def getStrongestSetting(self):
-        self.settings = self.algo.getAllSettings()
+        
+    def getStrongestSetting(self): 
+        self.settings = BruteForce(self.cards).getAllSettings()
         return self.settings[0]
 
     def getBestFitSetting(self, opponent):
         opponentBestSetting = opponent.getStrongestSetting()
-        self.settings = self.algo.getAllSettings()
+        self.settings = BruteForce(self.cards).getAllSettings()
         for setting in self.settings:
             if setting.compare(opponentBestSetting) == 1:
                 return setting
