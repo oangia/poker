a3438575e9c75b95768ef1f7623d5dcb088605ed
oangia <hqnhatdn@gmail.com>
2025-04-06 11:07:13 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,11 @@
 from setuptools import setup
 
-setup()
+setup(
+    name='poker',
+    version='0.1',
+    install_requires=[
+        'pydub',
+        'gTTS',
+        'SpeechRecognition'
+    ]
+)
