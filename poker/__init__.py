0908c12f9aac01852e00b28aa81990ac5dd8a0ba
oangia <hqnhatdn@gmail.com>
2025-04-12 16:50:09 +0700

    Update __init__.py

--- poker/__init__.py
+++ poker/__init__.py
@@ -7,8 +7,8 @@ from moviepy.editor import *
 from pydub import AudioSegment
 
 def make_video(video_path, audio, bg_music):
-    narrator = poker.adjust_volume(audio, target_dBFS=-28)
-    music = poker.adjust_volume(bg_music, target_dBFS=-39)
+    narrator = adjust_volume(audio, target_dBFS=-28)
+    music = adjust_volume(bg_music, target_dBFS=-39)
     final_audio = narrator.overlay(music)
     final_audio.export("output_audio.wav", format="wav")
     final_audio = AudioFileClip("output_audio.wav")
