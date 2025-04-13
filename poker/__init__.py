eeb5a6fd111d81e227251b08228b97ee589f4232
oangia <hqnhatdn@gmail.com>
2025-04-13 15:46:36 +0700

    Update __init__.py

--- poker/__init__.py
+++ poker/__init__.py
@@ -1,17 +1,5 @@
-from poker.Media import play_audio, play_video
 from poker.File import random_file, file_exists
-from poker.TTS import text_to_speech, speech_to_text
-from poker.Audio import adjust_volume, adjust_speed
+from poker.TTS import text_to_speech
 from poker.Subtitle import Subtitle
 from poker.Video import Video
-import numpy as np
-from moviepy.editor import *
-from pydub import AudioSegment
-
-def process_audio(audio, bg_music):
-    narrator = adjust_volume(audio, target_dBFS=-28)
-    music = adjust_volume(bg_music, target_dBFS=-39)
-    final_audio = narrator.overlay(music)
-    final_audio.export("output_audio.wav", format="wav")
-    final_audio = AudioFileClip("output_audio.wav")
-    return final_audio
+from poker.Audio import Audio
