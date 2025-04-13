d69a4ed9020f8df9fe5e3fb8998c2fd8aba172a1
oangia <hqnhatdn@gmail.com>
2025-04-13 16:01:24 +0700

    Delete poker/Media.py

--- poker/Media.py
+++
@@ -1,7 +0,0 @@
-from IPython.display import Audio, Video, display
-
-def play_audio(file, autoplay=True):
-    display(Audio(file, autoplay=autoplay))
-
-def play_video(file):
-    Video(file, embed=True)
