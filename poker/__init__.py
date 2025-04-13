c94cf500f5adb702d068e1470ad5840a79bff536
oangia <hqnhatdn@gmail.com>
2025-04-13 14:57:01 +0700

    Update __init__.py

--- poker/__init__.py
+++ poker/__init__.py
@@ -2,7 +2,7 @@ from poker.Media import play_audio, play_video
 from poker.File import random_file, file_exists
 from poker.TTS import text_to_speech, speech_to_text
 from poker.Audio import adjust_volume, adjust_speed
-from PIL import Image, ImageDraw, ImageFont
+from poker.Subtitle import Subtitle
 import numpy as np
 from moviepy.editor import *
 from pydub import AudioSegment
@@ -26,46 +26,3 @@ def process_video(video_path):
     video_resized = video_resized.crop(x1=(video_resized.w - target_width) // 2, x2=(video_resized.w + target_width) // 2)
     
     return video_resized
-
-def add_text_to_video(texts, video):
-    total_count = 0
-    for text in texts:
-        total_count += len(text)
-    
-
-    # Use default font
-    font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf", size=40)
-    text_clips = []
-    current_time = 0
-    # Create a text clip for each entry in the array
-    for text in texts:
-        # Create transparent image for each text clip
-        img = Image.new("RGBA", (video.w, video.h), (0, 0, 0, 0))
-        draw = ImageDraw.Draw(img)
-        text_duration = len(text) * video.duration / total_count
-        # Center the text
-        bbox = draw.textbbox((0, 0), text, font=font)
-        text_width = bbox[2] - bbox[0]
-        text_height = bbox[3] - bbox[1]
-        x = (video.w - text_width) // 2
-        y = (video.h - text_height) // 2
-
-        # Add a solid rectangle background behind the text (semi-transparent)
-        background_padding = 10  # Space between text and background
-        draw.rectangle(
-            [x - background_padding, y - background_padding, x + text_width + background_padding, y + text_height + background_padding],
-            fill=(0, 0, 0, 64)  # Semi-transparent black background (RGBA)
-        )
-
-        draw.text((x, y), text, font=font, fill="white")
-
-        # Convert image to clip
-        text_img = np.array(img)
-        txt_clip = ImageClip(text_img, duration=text_duration - 0.1).set_start(current_time)
-
-        # Add to the list of text clips
-        text_clips.append(txt_clip)
-        current_time += text_duration
-
-    return text_clips
-
